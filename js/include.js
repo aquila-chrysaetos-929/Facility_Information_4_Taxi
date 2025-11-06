fe65772494829465cc5c0b4bb79d1257fe26bb4c
aquila-chrysaetos-929 <93303801+aquila-chrysaetos-929@users.noreply.github.com>
2025-11-06 22:05:12 +0900

    Add files via upload

---
+++ js/include.js
@@ -0,0 +1,6 @@
+<script>
+document.querySelectorAll('[data-include]').forEach(async el=>{
+  const file=el.getAttribute('data-include');
+  const res=await fetch(file); el.innerHTML=await res.text();
+});
+</script>
